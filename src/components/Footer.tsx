1861bc003f738bb954185a423d32524f2c80fb5c
Alexander-Puv <sansano4sana@gmail.com>
2022-12-26 21:33:04 +0300

    starting with voice messages... and understanding they are not free. But I added a better way to handle messages sending

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,62 +1,61 @@
-import { Box, Button, Grid, TextField } from '@mui/material';
+import { Box, Button, Grid, IconButton, TextField } from '@mui/material';
 import { Timestamp, arrayUnion, doc, serverTimestamp, updateDoc } from 'firebase/firestore';
 import { useContext, useState, useRef } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { FirebaseContext } from '../MainConf';
 import { ChatContext } from '../reducer/ChatContext';
-import { Loader } from './UI';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import SendIcon from '@mui/icons-material/Send';
 import useTheme from '@mui/material/styles/useTheme';
 
 const Footer = () => {
   const {auth, firestore} = useContext(FirebaseContext)
   const [user] = useAuthState(auth)
   const [value, setValue] = useState('')
-  const [loading, setLoading] = useState(false)
   const chatContext = useContext(ChatContext)
   const ref = useRef<HTMLDivElement | null>(null)
   const theme = useTheme()
 
   const SendMessage = async () => {
-    if (!chatContext?.state) {
-      return
-    }
-    setLoading(true)
-
     if (value && chatContext?.state && chatContext.state.user && user){
+      const val = value
+      setValue('')
+
       await updateDoc(doc(firestore, 'chats', chatContext.state.chatId), {
         messages: arrayUnion({
           uid: user.uid,
           displayName: user.displayName,
           photoURL: user.photoURL,
-          text: value,
+          text: val,
           createdAt: Timestamp.now()
         })
       })
       
       await updateDoc(doc(firestore, 'userChats', chatContext.state.user.uid), {
         [chatContext.state.chatId + '.lastMessage']: {
-          value
+          value: val
         },
         [chatContext.state.chatId + '.date']: serverTimestamp()
       })
       await updateDoc(doc(firestore, 'userChats', user.uid), {
         [chatContext.state.chatId + '.lastMessage']: {
-          value
+          value: val
         },
         [chatContext.state.chatId + '.date']: serverTimestamp()
       })
-
-      setValue('')
     }
-    setLoading(false)
+  }
+
+  const VoiceMessage = async() => {
+
   }
 
   return (
     <Grid
       container alignSelf='flex-end'
       component='footer' position='absolute'
-      sx={{bottom: 0, right: 0, background: theme.palette.background.default}}
-      p={1}
+      sx={{bottom: 0, right: 0, /* background: theme.palette.background.default */}}
+      p={1} gap={1}
     >
       <TextField
         variant='outlined'
@@ -73,9 +72,14 @@ const Footer = () => {
         ref={ref}
       />
       <Box display='flex' alignItems='flex-end'>
-        {loading
-          ? <Loader p='0 10px' display='flex' />
-          : <Button onClick={() => SendMessage()} sx={{minHeight: 40, alignItems: 'center'}}>Send</Button>
+        {value ? 
+          <IconButton onClick={() => SendMessage()} sx={{color: theme.palette.primary.main}}>
+            <SendIcon />
+          </IconButton>
+        :
+          <IconButton onClick={() => VoiceMessage()} sx={{color: theme.palette.text.secondary}}>
+            <KeyboardVoiceIcon />
+          </IconButton>
         }
       </Box>
     </Grid>
